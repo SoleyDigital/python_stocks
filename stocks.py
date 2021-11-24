8673d39f81f5f2b7111144ea7e71713bcf7bff44
soleyman <shahirsoleyman@yahoo.com>
2021-11-24 10:41:33 +0000

    version 1 of python file

---
+++ stocks.py
@@ -0,0 +1,12 @@
+# DOCN Open
+docn_open = 99.24
+print('$DOCN Open', docn_open)
+# DOCN High Stock 
+docn_high = 104.86
+print('$DOCN High', docn_high)
+# DOCN Low Stock 
+docn_low = 94.74
+print('$DOCN Low', docn_low)
+# DOCN Closed
+docn_closed = 98.96
+print('$DOCN Closed', docn_low)
